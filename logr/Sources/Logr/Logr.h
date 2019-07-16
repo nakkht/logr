b66c56e3c6e72391ff5a41cbf8b494be2ffb6824
Paulius Gudonis <pg@neqsoft.com>
2019-07-16 23:09:16 +0200

    Introduced Sources package/folder

---
+++ logr/Sources/Logr/Logr.h
@@ -0,0 +1,19 @@
+//
+//  Logr.h
+//  Logr
+//
+//  Created by Paulius Gudonis on 16/07/2019.
+//  Copyright © 2019 neqsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Logr.
+FOUNDATION_EXPORT double LogrVersionNumber;
+
+//! Project version string for Logr.
+FOUNDATION_EXPORT const unsigned char LogrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logr/PublicHeader.h>
+
+
