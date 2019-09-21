376679bafbf41eda046ed7e7fdbd0b3adacedefc
Paulius Gudonis <pg@neqsoft.com>
2019-09-21 23:50:53 +0200

    Added Package.swift file and .gitignore

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Logr",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v9)
+    ],
+    products: [
+        .library(name: "Logr", targets: ["Logr"])
+    ],
+    targets: [
+        .target(name: "Logr",
+                path: ".",
+                sources: ["Sources"]),
+        .testTarget(name: "LogrTests",
+                    dependencies: ["Logr"],
+                    path: ".",
+                    sources: ["Tests"])
+    ]
+)
