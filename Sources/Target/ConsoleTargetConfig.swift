f0210566889e3fc075b0b45e0994b2062493645d
Paulius Gudonis <pg@neqsoft.com>
2019-10-27 22:56:18 +0100

    Added documentation for ConsoleTargetConfig

--- Sources/Target/ConsoleTargetConfig.swift
+++ Sources/Target/ConsoleTargetConfig.swift
@@ -8,13 +8,30 @@
 
 import Foundation
 
+/// Immutable struct used to encapsulate consolet target configuration values
 public struct ConsoleTargetConfig {
     
+    /// Subsystem name value assigned during initialization. Used for OSLog configuration.
     public let subsystem: String
+    
+    /// Category name value assigned during initialization. Used for OSLog configuration.
     public let category: String
+    
+    /// Logging levels value assigned during initialization.
     public let levels: [LogLevel]
+    
+    /// Logging style value assigned during initialization.
     public let style: Style
     
+    /**
+     Initialzes configuration struct with provided values.
+     
+     - Parameters:
+        - substystem: title of OSLog subsystem. Helps to distinguish logs within Console logs. Defaults to: com.neqsoft.logr
+        - category: title of OSLog category. Helps to distinguish logs within Console logs. Defaults to: ConsoleTarget
+        - levels: array of log levels, which shall be logged
+        - style: logging style. Defaults to: minimal style
+     */
     public init(subsystem: String? = nil, category: String? = nil, levels: [LogLevel]? = nil, style: Style? = nil) {
         self.subsystem = subsystem ?? "com.neqsoft.logr"
         self.category = category ?? "ConsoleTarget"
