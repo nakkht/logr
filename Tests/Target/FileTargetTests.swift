878c3dcf79c7642a6a30a233a36514da452bb292
Paulius Gudonis <pg@neqsoft.com>
2020-06-04 20:40:38 +0200

    Moved file targer config tests under file target tests

--- Tests/Target/FileTargetTests.swift
+++ Tests/Target/FileTargetTests.swift
@@ -20,15 +20,52 @@ import XCTest
 class FileTargetTests: XCTestCase {
     
     var fileTarget: FileTarget!
-
+    var targetConfig: FileTargetConfig!
+    
     override func setUp() {
         fileTarget = FileTarget()
         
         XCTAssertNotNil(fileTarget.fileHandle)
         XCTAssertNotNil(fileTarget.config)
+        
+        targetConfig = FileTargetConfig()
+        
+        XCTAssertEqual("log", targetConfig.fileExtension)
+        XCTAssertEqual("file", targetConfig.fileName)
+        XCTAssertEqual(TimeSpan.day, targetConfig.archiveFrequency)
+        XCTAssertEqual(FileTargetConfig.defaultMaxFileSizeInBytes, targetConfig.maxFileSizeInBytes)
+        XCTAssertEqual("file.log", targetConfig.fullFileName)
+        XCTAssertEqual("file.0.log", targetConfig.archiveFileName)
+        XCTAssertEqual(Style.minimal, targetConfig.style)
+        XCTAssertEqual(.debug, targetConfig.level)
+        XCTAssertEqual(1, targetConfig.maxArchivedFilesCount)
     }
-
+    
     override func tearDown() {
         fileTarget = nil
+        targetConfig = nil
+    }
+    
+    func testConfig() {
+        let size = UInt64(100 * 1024 * 1024)
+        let fileExtension = "longextension"
+        let fileName = "long-file-name"
+        targetConfig = FileTargetConfig(fileName: fileName, fileExtension: fileExtension, maxArchivedFilesCount: 100,
+                                        maxFileSizeInBytes: size ,style: .verbose)
+        
+        XCTAssertEqual(fileExtension, targetConfig.fileExtension)
+        XCTAssertEqual(fileName, targetConfig.fileName)
+        XCTAssertEqual(size, targetConfig.maxFileSizeInBytes)
+        XCTAssertEqual(.verbose, targetConfig.style)
+        XCTAssertEqual("\(fileName).\(fileExtension)", targetConfig.fullFileName)
+        XCTAssertEqual(100, targetConfig.maxArchivedFilesCount)
+        XCTAssertEqual("\(fileName).0.\(fileExtension)", targetConfig.archiveFileName)
+    }
+    
+    func testSetArchiveFrequency() {
+        TimeSpan.allCases.forEach {
+            targetConfig = FileTargetConfig(archiveFrequency: $0)
+            XCTAssertEqual($0, targetConfig.archiveFrequency)
+        }
     }
 }
